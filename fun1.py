9683f7b066a1b9f5ddd8eaefa6a8e83581a8479b
4Tipy <lchavez21@jaumebalmes.net>
2023-01-17 20:55:10 +0100

    avance imprimir columnas por nombre

--- fun1.py
+++ fun1.py
@@ -3,13 +3,17 @@ from matplotlib import pyplot as plt
 
 
 def mostrar():
-    df = pd.read_csv('df_covid19_countries2.csv', usecols=['location'])
-    print(df.head(50))
-    print("-----------------")
-    print(df.groupby("location").total_cases.sum())
+    df = pd.read_csv('df_covid19_countries2.csv')
+    df2 = df[['location', 'total_cases', 'date']]
 
+    print(df2)
 
     """""
+
+        df = pd.read_csv('df_covid19_countries2.csv')
+    print(df.loc[df['location', 'date'] == 'Afghanistan'])
+
+    df = pd.read_csv('df_covid19_countries2.csv', usecols=['location'])
     print(df.head(2))
     print("---------------------------")
     print(df.columns)
@@ -18,6 +22,18 @@ def mostrar():
     print("---------------------------")
     print(df['total_cases'].describe())
     print("---------------------------")
+    
+    arxiu = pd.read_csv("golejadors.csv")
+    player_goals = arxiu[(arxiu['Golejadors'] == 'Messi')]
+    
+    print(player_goals)
+    print(player_goals['Goals'])
+    print(player_goals['Goals].sum())
+    
+    
+    print(arxiu.groupby(by='Golejadors').mean()) para calcular la media
+    
+        print(df.groupby("location").total_cases.sum()) ta mal
     """
 
 
